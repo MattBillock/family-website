7ddd8220cb3a13779c156a8b1f2e7c9fa2b78899
Matt Billock <matt@billock.org>
2020-03-15 05:34:01 -0500

    Navigation to nowheresville

--- billock-org/src/components/NavBar.jsx
+++ billock-org/src/components/NavBar.jsx
@@ -3,12 +3,40 @@ import React, { Component } from 'react'
 export default class NavBar extends Component {
   render() {
     return (
+
+      
       <div>
-        <a href="matt.billock.org">Matt</a>
-        <a href="kiley.billock.org">Kiley</a>
-        <a href="luella.billock.org">Luella</a>
-        <a href="connor.billock.org">Connor</a>
-        <a href="house.billock.org">House</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="https://www.billock.org/#home">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.billock.org/#matt">Matt</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.billock.org/#kiley">Kiley</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.billock.org/#luella">Luella</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.billock.org/#connor">Connor</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="https://www.billock.org/#house">HJouse</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     )
   }
