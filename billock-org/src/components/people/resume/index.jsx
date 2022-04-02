3f33beb6995914dd68fbd84fc4fef65c9654bdf5
Matt Billock <matt@billock.org>
2022-04-02 17:12:44 -0500

    Trying PDF

--- billock-org/src/components/people/resume/index.jsx
+++ billock-org/src/components/people/resume/index.jsx
@@ -4,16 +4,26 @@ import EmploymentHistory from './EmploymentHistory'
 import Education from './Education'
 import OtherRelated from './OtherRelated'
 import '../../../stylesheets/resume.sass'
+import JsPDF from 'jspdf'
 
 export default class Resume extends Component {
+  generatePDF() {
+    const report = new JsPDF('portrait','pt','a4')
+    report.html(document.querySelector('#resume-content')).then( () => {
+      report.save('matthew-raymond-billock-resume.pdf')
+    })
+  }
   render() {
     return (
       <div className='resume'>
-        <h1>MATTHEW R. BILLOCK</h1>
-        <Skills />
-        <EmploymentHistory />
-        <Education />
-        <OtherRelated />
+        <button onClick={this.generatePDF} type='button'>Export PDF</button>
+        <div id="resume-content">
+          <h1>MATTHEW R. BILLOCK</h1>
+          <Skills />
+          <EmploymentHistory />
+          <Education />
+          <OtherRelated />
+        </div>
       </div>
     )
   }
